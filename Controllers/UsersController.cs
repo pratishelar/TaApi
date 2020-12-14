d5fc2a857a6acafdab7d617d12cb438fa8db9618
admin <admin@MacBook-Pro.local>
2020-12-14 11:30:23 -0500

    end of sec 10

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using Data;
@@ -55,6 +56,22 @@ namespace Controllers
             // var userToReturn = _mapper.Map<MemberDto>(user);
             
             return user;
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto){
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to Upate User");
         }
 
     }
---
+++ DTOs/MemberUpdateDto.cs
@@ -0,0 +1,16 @@
+namespace DTOs
+{
+    public class MemberUpdateDto
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string city { get; set; }
+
+        public string country { get; set; }
+
+        public string gender { get; set; }
+
+    }
+}
